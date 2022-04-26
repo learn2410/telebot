dc7451fb1d290601dfe90dfcdf6855f6f4cac5a7
learn2410 <learn2410@mail.ru>
2022-04-26 23:32:35 +0500

    created telebot.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# telebot
+# Telebot
+telegram bot - reports the result of checking homework on Devman.org by Telegram
+
+##Run bot
+You must already have Python 3 installed to run the bot.
+
+- Download code
+- Install dependencies with `pip install -r requirements.txt`
+- Specify required environment variables
+- Start the server with `python3 telebot.py`
---
+++ telebot.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+import requests
+import telegram
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+REVIEWS_URL = 'https://dvmn.org/api/user_reviews/'
+LONGPOLLING_URL = 'https://dvmn.org/api/long_polling/'
+
+
+def send_message(devman_response, token=TELEGRAM_TOKEN, chat_id=TELEGRAM_CHAT_ID):
+    bot = telegram.Bot(token=token)
+    name = 'Hi'
+    for post in bot.get_updates():
+        if post.message.from_user.id == chat_id:
+            name = post.message.from_user.name
+            break
+    lesson_title = devman_response['lesson_title']
+    lesson_url = devman_response['lesson_url']
+    submitted_at = devman_response['submitted_at'][:16].replace('T',' ')
+    result_text = 'Unfortunately, there were errors in the work.' if devman_response['is_negative']\
+        else 'The teacher liked everything, you can proceed to the next lesson!'
+    message_text = f'''\
+    {name}, your work is checked at {submitted_at}\r
+    *work name:* ["{lesson_title}"]({lesson_url})\r
+    *result:* {result_text}\n 
+    '''
+    bot.send_message(text=message_text, chat_id=int(chat_id),parse_mode=telegram.ParseMode.MARKDOWN)
+
+
+def main():
+    timestamp = time.time()-86400*6
+    while True:
+        try:
+            response = requests.get(LONGPOLLING_URL, allow_redirects=False, timeout=120,
+                                    headers={'Authorization': DEVMAN_TOKEN}, params={'timestamp': timestamp})
+            response.raise_for_status()
+            checked_tasks = response.json()
+            if 'status' not in checked_tasks:
+                continue
+            if checked_tasks['status'] == 'timeout':
+                timestamp = checked_tasks['timestamp_to_request']
+            elif checked_tasks['status'] == 'found':
+                timestamp = checked_tasks['last_attempt_timestamp']
+                for attempt in checked_tasks['new_attempts']:
+                    send_message(attempt)
+        except requests.exceptions.ReadTimeout:
+            pass
+        except requests.exceptions.ConnectionError:
+            time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
